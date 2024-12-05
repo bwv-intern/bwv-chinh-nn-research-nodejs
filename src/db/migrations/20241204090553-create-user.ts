f9246e057dedb4352a6241e88dba4822944f8535
bwv-chinh-nn <chinh.nn@briswell-vn.com>
2024-12-05 15:31:46 +0700

    new update not null migration file

--- src/db/migrations/20241204090553-create-user.ts
+++ src/db/migrations/20241204090553-create-user.ts
@@ -5,46 +5,37 @@ module.exports = {
   async up(queryInterface: QueryInterface, Sequelize:typeof DataTypes) {
     await queryInterface.createTable('Users', {
       id: {
+        allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER,
-        allowNull: false,
+        type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING,
-        allowNull: false,
+        type: Sequelize.STRING
       },
       phoneNumber: {
-        type: Sequelize.STRING,
-        allowNull: false,
+        type: Sequelize.STRING
       },
       email: {
-        type: Sequelize.STRING,
-        allowNull: false,
+        type: Sequelize.STRING
       },
       age: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
+        type: Sequelize.INTEGER
       },
       address: {
-        type: Sequelize.STRING,
-        allowNull: false,
+        type: Sequelize.STRING
       },
       gender: {
-        type: Sequelize.STRING,
-        allowNull: false,
+        type: Sequelize.STRING
       },
       office: {
-        type: Sequelize.STRING,
-        allowNull: false,
+        type: Sequelize.STRING
       },
       position: {
-        type: Sequelize.STRING,
-        allowNull: false,
+        type: Sequelize.STRING
       },
       startDate: {
-        type: Sequelize.DATEONLY,
-        allowNull: false,
+        type: Sequelize.DATEONLY
       },
       createdAt: {
         allowNull: false,
